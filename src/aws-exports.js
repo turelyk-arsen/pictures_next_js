16884da725fc7c9b61bc28cf3dd4d756e61a0cc2
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-08-16 20:50:07 +0200

    чсґ

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -20,8 +20,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000",
-        "redirectSignOut": "http://localhost:3000",
+        "redirectSignIn": "http://localhost:3000,https://pictures-next-js-git-aws-turelyk-arsen.vercel.app",
+        "redirectSignOut": "http://localhost:3000,https://pictures-next-js-git-aws-turelyk-arsen.vercel.app",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
